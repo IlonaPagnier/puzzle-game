cc2a1f25cc840dfd7c6ffa25c0cf47edc580491d
ipa <pagnier.ilona@gmail.com>
2024-02-09 15:52:51 +0100

    reverse+rotate

--- client/index.js
+++ client/index.js
@@ -8,23 +8,93 @@ let puzzle={
 [1,1,1,1,1,1]
     ],
     cat:"",    //categorie de la carte de l'user
-    pieces:[]   //liste des pieces accessibles a l'user
+    pieces:[]   //liste des pieces posées dans le puzzle
 };
 
-let pieceList=[ //liste des pieces existantes
-[[1]],
-[[1,1,1]],
-[[1,1,1,1]],
-[[1,0],[1,1]],
-[[1,0,0],[1,1,1]]
-];
+let pieceList={ //liste des pieces existantes
+    orange: [[1]],
+    jaune: [[1,1,1]],
+    violet: [[1,1,1,1]],
+    rouge: [[1,0],[1,1]],
+    bleu: [[1,0,0],[1,1,1]]
+};
 
 //TODO creer une fonction reverse ET rotate pour les pieces de l'utilisateur
 
 function showPuzzle(puzzle){
     for(l of puzzle.mat){
+        let t = ""
+        for(c of l) {
+            switch(c) {
+                case 0:
+                    t += "..";
+                    break;
+                case 1:
+                    t += "  ";
+                    break;
+                default:
+                    t += "##";
+            }
+        }
+        console.log(t);
+    }
+}
+
+
+function showPiece(piece){
+    for(l of piece){
         console.log(l);
     }
+}
+
+function rotatePiece(piece){ //sens horaire +90deg
+    let res=[];
+    for(i in piece[0]){
+        res.push([]);
+        for(j of piece){
+            res[i].unshift(j[i]);
+        }
+    }
+    return res;
+}
+
+function reversePiece(piece){ //sens horaire +90deg
+    let res=[];
+    for(i of piece){
+        res.unshift(i);
+    }
+    return res;
+}
+
+
+
+showPuzzle(puzzle);
+console.log("\n");
+
+for(p in pieceList){
+    showPiece(pieceList[p]);
+    console.log("\n");
+    showPiece(rotatePiece(pieceList[p]));
+    console.log("\n");
+    showPiece(reversePiece(pieceList[p]));
+    console.log("\n");
 
 }
-showPuzzle(puzzle);
+
+function fillPuzzle(puzzle,indexPiece,posx,posy, rotation, reverse) {
+    /* Args:
+    - puzzle : puzzle à completer
+    - indexPiece : index de la pièce à poser
+    - posx et posy = emplacement DANS puzzle
+    - rotation : nombre de rotation que la piece effectue (0 à 3)
+    - reverse : est-ce que la pièce fait un reverse (0 ou 1)
+*/
+
+    // Recréation de la matrice de la pièce
+
+
+    // verifier les bords
+
+
+    // Boucles for pour vérifier si la pièce peut être posée
+}
